39292adb9fb65e2ef44d768afa24a5b3d1e5370d
Robin van Baalen <robin@ganttify.com>
2025-11-21 19:02:01 -0400

    Initial Commit

---
+++ SpreadPaper/SpreadPaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpreadPaperApp.swift
+//  SpreadPaper
+//
+//  Created by Robin van Baalen on 21-11-2025.
+//
+
+import SwiftUI
+
+@main
+struct SpreadPaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
